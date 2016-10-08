9a20978847beae7c0060dd830e22244d6fc6bc27
Fred Wang <namelos@outlook.com>
2016-10-08 22:45:21 +0800

    add xunit

---
+++ CashRegister/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using static Xunit.Assert;
+
+namespace CashRegister
+{
+    public class Test
+    {
+        [Fact]
+        public void Truth() => True(true);
+    }
+}
